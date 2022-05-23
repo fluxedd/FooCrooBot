7dad2e0c2531039faa4b862b4eb867e85f686b2a
Mark Miranda <mark.miiranda16@gmail.com>
2022-05-23 15:56:30 -0500

    message handler

--- main.py
+++ main.py
@@ -1,8 +1,21 @@
-from telegram import Update 
-from telegram.ext import Updater, CommandHandler
+from telegram import InlineKeyboardButton, Update 
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup
 import os
 
 API_KEY = os.getenv('API_KEY')
 
+def start(update, context):
+    update.message.reply_text("Hello. Welcome to Foo'Croo Bot.")
+
+def bot_reply(update, context):
+    update.message.reply_text(f"You said {update.effective_user.first_name}")
+
 updater = Updater(API_KEY)
 dispatcher = updater.dispatcher
+
+dispatcher.add_handler(CommandHandler("start", start))
+dispatcher.add_handler(MessageHandler(Filters.text, bot_reply))
+updater.start_polling()
+updater.idle()
+
