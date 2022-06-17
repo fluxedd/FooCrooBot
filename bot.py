7428216f39bbf1b1764569fc7d86bfa97c7522a9
Mark Miranda <mark.miiranda16@gmail.com>
2022-06-17 18:34:51 -0500

    deployment

--- bot.py
+++ bot.py
@@ -41,7 +41,7 @@ def sq_choice(update: Update, context: CallbackContext) -> int:
     return USER_CHOICE
 
 def quest_details(update: Update, context: CallbackContext) -> int:
-    context.bot_data['choice'] = update.message.text
+    context.chat_data['choice'] = update.message.text
     reply_text = f'Who are the participants to go to {update.message.text}?'
 
     update.message.reply_text(reply_text)
@@ -50,14 +50,14 @@ def quest_details(update: Update, context: CallbackContext) -> int:
 
 def log_list(update: Update, context: CallbackContext) -> None:
     update.message.reply_text(
-        text=f"<b><u>Foo'Croo Log List</u></b>\n{format_log(context.bot_data)}",
+        text=f"<b><u>Foo'Croo Log List</u></b>\n{format_log(context.chat_data)}",
         parse_mode='HTML'
     )
 
 def log_info(update: Update, context: CallbackContext) -> int:
-    category = context.bot_data["choice"]
-    context.bot_data[category] = update.message.text
-    del context.bot_data["choice"]
+    category = context.chat_data["choice"]
+    context.chat_data[category] = update.message.text
+    del context.chat_data["choice"]
 
     update.message.reply_text(
         text="<b><u>SUCCESSFULLY LOGGED</u></b>\n\nUse /logs to view the log list.",
@@ -67,8 +67,8 @@ def log_info(update: Update, context: CallbackContext) -> int:
     return ConversationHandler.END
 
 def done(update: Update, context: CallbackContext) -> int:
-    if 'choice' in context.bot_data:
-        del context.bot_data['choice']
+    if 'choice' in context.chat_data:
+        del context.chat_data['choice']
 
     update.message.reply_text(
         text="<b><u>SUCCESSFULLY LOGGED</u></b>\n\nUse /logs to view the log list.",
@@ -103,7 +103,7 @@ def delete_entry(update: Update, context: CallbackContext):
     return USER_CHOICE
 
 def confirm_delete(update: Update, context: CallbackContext):
-    context.bot_data.pop(update.message.text)
+    context.chat_data.pop(update.message.text)
     update.message.reply_text(
         text=f"{update.message.text} - <b><u>SUCCESSFULLY REMOVED</u></b>\n\nUse /logs to view the log list.",
         parse_mode='HTML'
@@ -113,7 +113,7 @@ def confirm_delete(update: Update, context: CallbackContext):
 
 def main() -> None:
     persistence = PicklePersistence(filename='loglist')
-    updater = Updater("5347268144:AAFZKtrWdNNQQpFD6QpmEal6RbjRThnxvZo", persistence=persistence)
+    updater = Updater(os.environ.get('TOKEN'), persistence=persistence)
 
     dispatcher = updater.dispatcher
 
