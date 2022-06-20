a0cdaef8f46e743a0e1a91ede1447dc0802f03fe
Mark Miranda <mark.miiranda16@gmail.com>
2022-06-20 12:28:09 -0500

    deploy

--- ping.js
+++ ping.js
@@ -1,7 +1,8 @@
 function wakeup() {
-  require("open")("https://fierce-sierra-52458.herokuapp.com", (err) => {
-    if (err) throw err;
-    console.log("Woke up!");
-    setTimeout(wakeup, 1740000); //29m
-  });
+  setInterval(ping(), 300000);
+}
+
+function ping() {
+  var http = require("http");
+  http.get("http://fierce-sierra-52458.herokuapp.com");
 }
